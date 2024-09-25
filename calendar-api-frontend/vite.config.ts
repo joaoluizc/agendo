a6e816033f97dc79f67fd249b7f0f6a854fcdcae
joaoluizc <joaocoelho.tdm@gmail.com>
2024-09-25 10:49:19 -0300

    update backend URI on front

--- calendar-api-frontend/vite.config.ts
+++ calendar-api-frontend/vite.config.ts
@@ -19,7 +19,8 @@ export default defineConfig({
         // secure: true, // Set this to `true` if the target server uses a valid SSL certificate
       },
       '/api': {
-        target: 'http://calendar-api-backend:3001',
+        // target: 'http://calendar-api-backend:3001',
+        target: 'https://agendo-backend.onrender.com/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), // Adjust the rewrite as necessary
         // secure: true, // Set this to `true` if the target server uses a valid SSL certificate
