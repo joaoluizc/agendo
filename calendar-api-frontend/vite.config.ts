4b984b11bf36d161500e73adca78bd8a3f23ee2d
joaoluizc <joaocoelho.tdm@gmail.com>
2024-09-25 10:25:55 -0300

    Revert "updating backend url on viteconfig"
    This reverts commit fead7f359f87e14fdcebc1ba318890f21803657e.

--- calendar-api-frontend/vite.config.ts
+++ calendar-api-frontend/vite.config.ts
@@ -19,8 +19,7 @@ export default defineConfig({
         // secure: true, // Set this to `true` if the target server uses a valid SSL certificate
       },
       '/api': {
-        // target: 'http://calendar-api-backend:3001',
-        target: 'https://agendo-backend.onrender.com',
+        target: 'http://calendar-api-backend:3001',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), // Adjust the rewrite as necessary
         // secure: true, // Set this to `true` if the target server uses a valid SSL certificate
