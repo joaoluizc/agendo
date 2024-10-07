4f67457945dc760c83c4954de213ba9625f350cc
joaoluizc <joaocoelho.tdm@gmail.com>
2024-10-07 13:00:00 -0300

    optim: set backend url as env variable on frontend

--- calendar-api-frontend/vite.config.ts
+++ calendar-api-frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'process'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,8 +20,7 @@ export default defineConfig({
         // secure: true, // Set this to `true` if the target server uses a valid SSL certificate
       },
       '/api': {
-        target: 'http://calendar-api-backend:3001',
-        // target: 'https://agendo-backend.onrender.com/',
+        target: process.env.BACK_END_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), // Adjust the rewrite as necessary
         // secure: true, // Set this to `true` if the target server uses a valid SSL certificate
