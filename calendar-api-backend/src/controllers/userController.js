61aef89dce2beff64ae39c2d6b19d7a1d781b373
Joao Coelho <joaocoelho.tdm@gmail.com>
2024-10-17 22:45:16 -0300

    update logout cookie to have a domain setting

--- calendar-api-backend/src/controllers/userController.js
+++ calendar-api-backend/src/controllers/userController.js
@@ -1,15 +1,17 @@
-import userService from '../services/userService.js'
-import bcrypt from 'bcrypt'
-import { sendCookies } from '../middlewares/sendCookies.js';
-import process from 'process';
+import userService from "../services/userService.js";
+import bcrypt from "bcrypt";
+import { sendCookies } from "../middlewares/sendCookies.js";
+import process from "process";
 
 const registerUser = async (req, res) => {
-  const {firstName, lastName, email, password, } = req.body;
+  const { firstName, lastName, email, password } = req.body;
   if (!firstName || !lastName || !email || !password) {
-    return res.status(400).json({message: "firstName, lastName, email, and password are required fields"});
+    return res.status(400).json({
+      message: "firstName, lastName, email, and password are required fields",
+    });
   }
   try {
-    console.log({ firstName, lastName, email, password});
+    console.log({ firstName, lastName, email, password });
     await userService.createUser(req.body);
     sendCookies(req, res);
   } catch (err) {
@@ -24,35 +26,44 @@ const loginUser = async (req, res) => {
   try {
     const user = await userService.findUser(email);
     if (!user) {
-      return res.status(400).json({ msg: 'Invalid credentials. Email not found.' });
+      return res
+        .status(400)
+        .json({ msg: "Invalid credentials. Email not found." });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ msg: 'Invalid credentials. Wrong password.' });
+      return res
+        .status(400)
+        .json({ msg: "Invalid credentials. Wrong password." });
     }
 
     sendCookies(req, res);
   } catch (err) {
     console.error(err.message);
-    res.status(500).json({message: `caught error: ${err.message}`});
+    res.status(500).json({ message: `caught error: ${err.message}` });
   }
 };
 
 const logoutUser = async (req, res) => {
-  res.cookie('jwt', '', { maxAge: 1, httpOnly: true, secure: process.env.NODE_ENV === 'production', });
-  res.status(200).send('Logged out');
+  res.cookie("jwt", "", {
+    maxAge: 1,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    domain: "dmsupport.org",
+  });
+  res.status(200).send("Logged out");
 };
 
 const userInfo = async (req, res) => {
   const userEmail = req.user.email;
   console.log(userEmail);
   if (!userEmail) {
-    return res.status(400).json({message: 'email is required'});
+    return res.status(400).json({ message: "email is required" });
   }
   const user = await userService.findUser(userEmail);
   if (!user) {
-    return res.status(400).json({message: 'User not found'});
+    return res.status(400).json({ message: "User not found" });
   }
   const response = {
     email: user.email,
