71bbfceddcd0c9a489cde414823506fc1d4e0d21
Joao Coelho <joaocoelho.tdm@gmail.com>
2024-11-21 15:29:17 -0300

    chore: revert userRouter /info route to not use clerk auth yet

--- calendar-api-backend/src/routers/userRouter.js
+++ calendar-api-backend/src/routers/userRouter.js
@@ -1,13 +1,14 @@
 import express from "express";
 import userController from "../controllers/userController.js";
-import { requireAuth } from "@clerk/express";
+// import { requireAuth } from "@clerk/express";
+import { verifyUserAuth } from "../middlewares/authMiddleware.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", userController.registerUser);
 userRouter.post("/login", userController.loginUser);
 userRouter.post("/logout", userController.logoutUser);
-userRouter.get("/info", requireAuth, userController.userInfo);
+userRouter.get("/info", verifyUserAuth, userController.userInfo);
 userRouter.post("/clerk/new", userController.newClerkUser);
 
 export default userRouter;
