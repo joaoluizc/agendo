eefe6c73f095d375b4062d1b26c18ce39a32e7d4
Joao Coelho <joaocoelho.tdm@gmail.com>
2024-11-21 11:18:15 -0300

    fix: refactor getting all users' tokens with clerk

--- calendar-api-backend/src/routers/userRouter.js
+++ calendar-api-backend/src/routers/userRouter.js
@@ -1,13 +1,13 @@
 import express from "express";
 import userController from "../controllers/userController.js";
-import verifyUserAuth from "../middlewares/verifyUserAuth.js";
+import { requireAuth } from "@clerk/express";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", userController.registerUser);
 userRouter.post("/login", userController.loginUser);
 userRouter.post("/logout", userController.logoutUser);
-userRouter.get("/info", verifyUserAuth, userController.userInfo);
+userRouter.get("/info", requireAuth, userController.userInfo);
 userRouter.post("/clerk/new", userController.newClerkUser);
 
 export default userRouter;
