8bd6845a7984b4f3ceb01ec4486d4417c76a5027
Joao Coelho <joaocoelho.tdm@gmail.com>
2025-02-18 13:22:24 -0300

    troubleshoot 2

--- calendar-api-frontend/src/components/ScheduleCalendar/CreateShiftBtn.tsx
+++ calendar-api-frontend/src/components/ScheduleCalendar/CreateShiftBtn.tsx
@@ -253,6 +253,16 @@ export default function NewShiftForm({ selectedDate }: NewShiftFormProps) {
       console.log("endTime: ", endTime);
       console.log("startTime toISOString: ", new Date(startTime).toISOString());
       console.log("endTime toISOString: ", new Date(endTime).toISOString());
+      const startTimeParsed = chrono.parseDate(startTime) || new Date();
+      const endTimeParsed = chrono.parseDate(endTime) || new Date();
+      console.log(
+        "startTime post-chrono: ",
+        new Date(startTimeParsed).toISOString()
+      );
+      console.log(
+        "endTime post-chrono: ",
+        new Date(endTimeParsed).toISOString()
+      );
 
       const newShift: NewShift = {
         startTime: new Date(startTime).toISOString(),
