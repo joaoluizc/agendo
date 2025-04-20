b0c45ad1ba6d5bac2fe10820e551805ca89b0a4c
Joao Coelho <joaocoelho.tdm@gmail.com>
2025-04-20 02:09:05 -0300

    feat: refactor ToggleBulkSelector to utilize user settings for admin type check

--- calendar-api-frontend/src/components/ScheduleCalendar/calendar-components/ToggleBulkSelector.tsx
+++ calendar-api-frontend/src/components/ScheduleCalendar/calendar-components/ToggleBulkSelector.tsx
@@ -3,8 +3,10 @@ import { useSchedule } from "@/providers/useSchedule";
 // import BulkCopyBtn from "./BulkCopyBtn";
 import BulkDeleteBtn from "./BulkDeleteBtn";
 import BulkDeselectBtn from "./BulkDeselectBtn";
+import { useUserSettings } from "@/providers/useUserSettings";
 
 function ToggleBulkSelector() {
+  const { type } = useUserSettings();
   const { isBulkSelectorActive, setIsBulkSelectorActive } = useSchedule();
 
   const toggleBulkSelector = () => {
@@ -12,27 +14,29 @@ function ToggleBulkSelector() {
   };
 
   return (
-    <div
-      className={`flex space-x-3 ml-auto p-2 rounded-md border border-border px-4 ${
-        isBulkSelectorActive ? "pl-1" : ""
-      }`}
-    >
-      {isBulkSelectorActive && (
-        <div id="bulk-selector-active-buttons" className="h-5">
-          {/* <BulkCopyBtn /> */}
-          <BulkDeselectBtn />
-          <BulkDeleteBtn />
+    type === "admin" && (
+      <div
+        className={`flex space-x-3 ml-auto p-2 rounded-md border border-border px-4 ${
+          isBulkSelectorActive ? "pl-1" : ""
+        }`}
+      >
+        {isBulkSelectorActive && (
+          <div id="bulk-selector-active-buttons" className="h-5">
+            {/* <BulkCopyBtn /> */}
+            <BulkDeselectBtn />
+            <BulkDeleteBtn />
+          </div>
+        )}
+        <div className="flex items-center space-x-2">
+          <Switch
+            id="bulk-selector-toggle"
+            checked={isBulkSelectorActive}
+            onCheckedChange={toggleBulkSelector}
+          />
+          <p className="text-sm">Bulk Select</p>
         </div>
-      )}
-      <div className="flex items-center space-x-2">
-        <Switch
-          id="bulk-selector-toggle"
-          checked={isBulkSelectorActive}
-          onCheckedChange={toggleBulkSelector}
-        />
-        <p className="text-sm">Bulk Select</p>
       </div>
-    </div>
+    )
   );
 }
 
