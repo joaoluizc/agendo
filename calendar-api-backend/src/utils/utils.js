a2fd5e2ac41c687ed03143c4e1fb9fdbe3297c6f
Joao Coelho <joaocoelho.tdm@gmail.com>
2025-04-23 13:22:05 -0300

    refactor: when creating a new user, get all current sling users to add user details to clerk instead of hard-coded list of users

--- calendar-api-backend/src/utils/utils.js
+++ calendar-api-backend/src/utils/utils.js
@@ -1,47 +1,57 @@
-import { slingUsers } from "./slingUsers.js";
+import SlingService from "../services/slingService.js";
 
 const todayISO = (date) => {
-    // Get today's date
-    const today = new Date(date);
-    today.setHours(0, 0, 0, 0); // Set the time to midnight (00:00:00.000)
+  // Get today's date
+  const today = new Date(date);
+  today.setHours(0, 0, 0, 0); // Set the time to midnight (00:00:00.000)
 
-    // Convert to ISO string
-    const startOfDayISO = today.toISOString();
+  // Convert to ISO string
+  const startOfDayISO = today.toISOString();
 
-    // Create a Date object for 24 hours later
-    const endOfDay = new Date(today);
-    endOfDay.setHours(24, 0, 0, 0); // Set the time to 24 hours later (midnight next day)
+  // Create a Date object for 24 hours later
+  const endOfDay = new Date(today);
+  endOfDay.setHours(24, 0, 0, 0); // Set the time to 24 hours later (midnight next day)
 
-    // Convert to ISO string
-    const endOfDayISO = endOfDay.toISOString();
+  // Convert to ISO string
+  const endOfDayISO = endOfDay.toISOString();
 
-    return `${startOfDayISO}/${endOfDayISO}`;
-}
+  return `${startOfDayISO}/${endOfDayISO}`;
+};
 
 const shiftToEvent = (shift) => {
-    const event = {
-        summary: shift.position.name,
-        // description: 'optional',
-        start: {
-            dateTime: shift.dtstart,
-            timeZone: 'Brazil/East'
-        },
-        end: {
-            dateTime: shift.dtend,
-            timeZone: 'Brazil/East'
-        },
-    };
-    return event;
+  const event = {
+    summary: shift.position.name,
+    // description: 'optional',
+    start: {
+      dateTime: shift.dtstart,
+      timeZone: "Brazil/East",
+    },
+    end: {
+      dateTime: shift.dtend,
+      timeZone: "Brazil/East",
+    },
+  };
+  return event;
 };
 
 const getSlingIdByEmail = (email) => {
-    const user = slingUsers.find(user => user.email === email);
-    return user ? user.slingId : undefined;
-}
+  const slingService = new SlingService();
+  slingService.init();
+  const slingUsers = slingService.users;
 
+  if (!slingUsers) {
+    console.error(
+      "Sling users not found. Please check if SlingService is initialized correctly."
+    );
+    return undefined;
+  }
+
+  const user = slingUsers.find((user) => user.email === email);
+  return user ? user.slingId : undefined;
+};
 
 export default {
-    todayISO,
-    shiftToEvent,
-    getSlingIdByEmail,
+  todayISO,
+  shiftToEvent,
+  getSlingIdByEmail,
 };
