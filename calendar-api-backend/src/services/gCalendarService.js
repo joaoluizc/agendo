5f840acda1b8db8ab284a37cf9a1890fe27ccdf6
Joao Luiz <joaocoelho.tdm@gmail.com>
2026-02-12 00:26:11 -0300

    fix: delayig map to getUserPositionsToSync respons e to avoid error when user has no positionsToSync

--- calendar-api-backend/src/services/gCalendarService.js
+++ calendar-api-backend/src/services/gCalendarService.js
@@ -545,9 +545,11 @@ const addDaysShiftsToGcal_cl = async (date, requestId = "req-id-nd") => {
         console.log(
           `[${requestId}] - Filtering shifts for ${user.firstName} to what user wants to sync`,
         );
-        const positionsToSync = await positionService
-          .getUserPositionsToSync(user.id)
-          .map((position) => position.positionId.toString());
+        const positionsToSyncResponse =
+          await positionService.getUserPositionsToSync(user.id);
+        const positionsToSync = positionsToSyncResponse.map((position) =>
+          position.positionId.toString(),
+        );
         // const positionsToSync = user.publicMetadata.positionsToSync.map(
         //   (position) => position.positionId.toString(),
         // );
