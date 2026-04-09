d9d19606d016e9a9dc26585c1bfbafb56a28eb89
Joao Luiz <joaocoelho.tdm@gmail.com>
2026-04-09 14:56:17 -0300

    fix: update footer to display the current year dynamically

--- calendar-api-frontend/src/components/Footer/Footer.tsx
+++ calendar-api-frontend/src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <div className="py-8" style={{ height: "10vh" }}>
       <div className="container mx-auto flex h-full items-center justify-center gap-5">
-        <p>© 2024 All Rights Reserved.</p>
+        <p>© {new Date().getFullYear()} All Rights Reserved.</p>
         <a href="/privacy" className="underline">
           Privacy Policy
         </a>
